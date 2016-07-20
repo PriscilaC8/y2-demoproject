5c801cab091580128015a4d836ec267b13324f27
Priscila Cortez <priscila@mit.edu>
2016-07-20 11:41:59 +0300

    adding google maps config stuff

--- web_app.py
+++ web_app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map, icons
 app = Flask(__name__)
+
+# Google Maps Config 
+app.config['GOOGLEMAPS_KEY'] = "AIzaSyDkbYFo8lquHs3dhFxiH0RTTgqt-epz1-0"
+GoogleMaps(app)
 
 # SQLAlchemy stuff
 from database_setup import Base, Comment, Commenter, Vote
@@ -13,8 +19,8 @@ session = DBSession()
 
 #YOUR WEB APP CODE GOES HERE
 @app.route("/")
-def main():
-	return render_template('index.html')
+def main():    
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
